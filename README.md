45dd86ffb6634687bc503dd885ae712d9e062077
Ben Koala <bkoala123@hotmail.com>
2021-07-03 10:18:44 -0700

    Password generator updates and files

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Password_generator
-An application to generate random passwords based on user selection
+This is an application to generate random passwords based on user criteria selection
+The user clicks on the Generate password to start the program.
+The user is prompted for criterias for length, lower case, uppercase, numeric or special characters to be included in the pool of characters from which a random password is generated.
+For each of the criteria, the user is repeately prompted until they enter the answer in the right format,or an answer that meets the criteria.  The user has the option of cancelling to terminate the program.
+When all correct answered have been provided, the random password is generated and displayed to the user.
---
+++ js/script.js
@@ -0,0 +1,103 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+//Generate a random word of a certain length from a string of characters 
+function randomWord (ll,ranChars){
+   var temp1="";
+   //Add all existing alphatetic characters to a standard string to choose from
+   for ( var xx=0; xx<ll; xx++) {
+     temp1 += ranChars.charAt(Math.floor(Math.random()*ranChars.length));
+   }  
+   //console.log (" Temp is " + temp1);
+   return temp1;
+}
+//Generate the password based on the user inputs
+function generatePassword(){
+  //Basic alphatetic characters
+  var useChars="abcdefghijklimnopqrstuvwxyz";
+  //Prompt user for password length until correct value is entered or user cancel program
+  var passLength =window.prompt("Please select the length of your password-From 8 to 128");
+ if (passLength){
+  while (isNaN(passLength) || passLength < 8 || passLength > 128 ) {
+    var passLength =window.prompt("Please enter a valid number  from 8 to 128");
+  }
+} else { alert("You terminated the program");
+   return;
+   }
+  //Prompt user for lower case choice until right answer is entered or exits
+ var pasL =window.prompt("Do you want to include a lower case in your password? yes or no" );
+ if (pasL){ 
+  pasL = pasL.toLowerCase();
+  while (pasL !== "yes" && pasL !== "no") {
+    var pasL =window.prompt("Please enter a valid answer for lower case; yes or no");
+  }
+} else { alert("You terminated the program");
+   return;
+   }
+//Prompt user for Upper case choice until right answer is entered or exits
+var pasU =window.prompt("Do you want to include a upper case in your password? yes or no" );
+if (pasU){ 
+  pasU = pasU.toLowerCase();
+ while (pasU !== "yes" &&  pasU !== "no") {
+   var pasU =window.prompt("Please enter a valid answer for your upper case; yes or no");
+ }
+} else { alert("You terminated the program");
+  return;
+  }
+ //Prompt user for number choice until right answer is entered or exits
+ var pasN =window.prompt("Do you want to include one number in your password? yes or no");
+if (pasN){ 
+  pasN = pasN.toLowerCase();
+ while (pasN !== "yes" && pasN !== "no") {
+   var pasN =window.prompt("Please enter a valid answer for number. yes or no");
+ }
+} else { alert("You terminated the program");
+  return;
+  } 
+//Prompt user for special character choice until right answer is entered for special character
+ 
+var pasS =window.prompt("Do you want to include one special character in your password? yes or no");
+    
+if (pasS){ 
+  pasS = pasS.toLowerCase();
+ while (pasS !== "yes" && pasS !== "no") {
+   var pasS =window.prompt("Please enter a valid answer for special character. yes or no");
+ }
+} else { alert("You terminated the program");
+  return;
+  } 
+  
+  // Make Modification to pool of characaters based on user criteria 
+  //No lower cases
+    if (  pasL ==="no") {
+    Lcase="abcdefghijklimnopqrstuvwxyz";
+    useChars= Lcase.toUpperCase();
+  }
+  //Upper cases included in pool
+  if ( pasU==="yes" ){
+    Lcase="abcdefghijklimnopqrstuvwxyz";
+    useChars= useChars +  Lcase.toUpperCase();
+  }
+  //Special characters included in pool
+  if ( pasS === "yes" ){
+  useChars= useChars + "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+}
+//Number included in the pool
+if ( pasN === "yes" ){
+  useChars= useChars + "0123456789";
+}
+//Generate a random word with the criterias from the updated pool of characters
+    randW=randomWord(passLength,useChars);
+ // Return password   
+ return randW;
+}
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
